0260d5928dfffbe4b6b96bdf7862a3cfee585d74
huoyo <1729913829@qq.com>
2021-01-29 22:08:02 +0800

    修复启动时未创建索引目录报错的bug

--- src/main/java/cn/langpy/simsearch/config/IndexConfig.java
+++ src/main/java/cn/langpy/simsearch/config/IndexConfig.java
@@ -2,7 +2,6 @@ package cn.langpy.simsearch.config;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.*;
 import org.apache.lucene.search.ControlledRealTimeReopenThread;
-import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.SearcherFactory;
 import org.apache.lucene.search.SearcherManager;
 import org.apache.lucene.store.Directory;
@@ -15,15 +14,15 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.util.StringUtils;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.concurrent.Executor;
+
+import static java.io.File.separator;
 
 
 @Configuration
 @EnableAsync
 public class IndexConfig {
-    String indexLocalDir=System.getProperty("user.dir")+"/indexs";
+    String indexLocalDir=System.getProperty("user.dir")+separator+"indexs";
 
     @Value("${sim-search.dir:}")
     String indexDir;
@@ -41,27 +40,8 @@ public class IndexConfig {
         if (StringUtils.isEmpty(indexDir)) {
             indexDir = indexLocalDir;
         }
-        Path path = Paths.get(indexDir);
-        File file = path.toFile();
-        if(!file.exists()) {
-            file.mkdirs();
-        }
+        File file = new File(indexDir);
         return FSDirectory.open(file);
-    }
-    @Bean
-    public IndexReader reader(Directory directory) {
-        IndexReader reader = null;
-        try {
-            reader = DirectoryReader.open(directory);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return reader;
-    }
-    @Bean
-    public IndexSearcher searcher(IndexReader reader) {
-        IndexSearcher searcher = new IndexSearcher(reader);
-        return searcher;
     }
 
     @Bean
@@ -80,7 +60,7 @@ public class IndexConfig {
         SearcherManager searcherManager = new SearcherManager(indexWriter, false, new SearcherFactory());
         ControlledRealTimeReopenThread cRTReopenThead = new ControlledRealTimeReopenThread(new TrackingIndexWriter(indexWriter), searcherManager, 5.0, 0.025);
         cRTReopenThead.setDaemon(true);
-        cRTReopenThead.setName("更新IndexReader线程");
+        cRTReopenThead.setName("Thread-update IndexReader");
         cRTReopenThead.start();
         return searcherManager;
     }
--- src/main/java/cn/langpy/simsearch/task/IndexTask.java
+++ src/main/java/cn/langpy/simsearch/task/IndexTask.java
@@ -17,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.beans.IntrospectionException;
