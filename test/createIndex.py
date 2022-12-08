2c14ab807b7b5314a44a53588ced04e5935f08f5
huoyo <1729913829@qq.com>
2022-12-08 10:30:23 +0800

    add python code to create indexes

--- test/createIndex.py
+++ test/createIndex.py
@@ -1,7 +1,7 @@
 import requests
 from tqdm import tqdm
-
-with open("text.txt",'r',encoding='utf-8') as file:
+# start simsearchdemo and create indexes by this code
+with open("text.min.txt",'r',encoding='utf-8') as file:
     se = requests.session()
     for i,line in tqdm(enumerate(file.readlines())):
         id, name = line.split(",")
